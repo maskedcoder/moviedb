b22fba7f3020b6dfd6fc62dcd4b8ad20e25a77ad
unknown <Tom@.(none)>
2013-04-24 13:38:12 -0500

    Improved back to previous page and mobile stylesheets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :log_back_url, :only => 'index'
+  
+  def log_back_url
+    if request.format == "html"
+      match = /\/\/.*?(\/.*)/.match(request.url)
+      session[:back] = match ? match[1] : "/"
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def back_url
+    return session[:back] || "/" + controller_name
+  end
 end
