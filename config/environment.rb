8a16df46b22dc1a92a919de470f0e600645cb5b1
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-12-29 17:55:23 -0600

    Automatically open web browser on server start

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Moviedb::Application.initialize!
+
+# Open a browser window
+# See http://stackoverflow.com/a/14053693
+link = "http://localhost:3000"
+if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
+  system "start #{link}"
+elsif RbConfig::CONFIG['host_os'] =~ /darwin/
+  system "open #{link}"
+elsif RbConfig::CONFIG['host_os'] =~ /linux|bsd/
+  system "xdg-open #{link}"
+end
